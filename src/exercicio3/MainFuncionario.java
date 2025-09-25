cb4c86827a3c8f7778fd4237d7adfd103809415e
Raquel Roessing <raquel.f.roessing@hotmail.com>
2025-09-25 04:10:01 -0400

    fix: Ex. 3 - main não captura mensagem em caso de erro, teste para salario negativo faltando

--- src/exercicio3/MainFuncionario.java
+++ src/exercicio3/MainFuncionario.java
@@ -6,20 +6,37 @@ import java.util.List;
 
 public class MainFuncionario {
     public static void main(String[] args) {
-        List<Funcionario> funcionarios = new ArrayList<>();
+        try {
+            List<Funcionario> funcionarios = new ArrayList<>();
 
-        Funcionario gerente1 = new Gerente("Angela", new BigDecimal("12000"));
-        Funcionario gerente2 = new Gerente("Bruno", new BigDecimal("10000"));
-        Funcionario dev1 = new Desenvolvedor("Carla", new BigDecimal("6000"));
-        Funcionario dev2 = new Desenvolvedor("Diogo", new BigDecimal("7500"));
+            Funcionario gerente1 = new Gerente("Angela", new BigDecimal("12000"));
+            Funcionario gerente2 = new Gerente("Bruno", new BigDecimal("10000"));
+            Funcionario dev1 = new Desenvolvedor("Carla", new BigDecimal("6000"));
+            Funcionario dev2 = new Desenvolvedor("Diogo", new BigDecimal("7500"));
 
-        funcionarios.add(gerente1);
-        funcionarios.add(gerente2);
-        funcionarios.add(dev1);
-        funcionarios.add(dev2);
+            funcionarios.add(gerente1);
+            funcionarios.add(gerente2);
+            funcionarios.add(dev1);
+            funcionarios.add(dev2);
 
-        for (Funcionario func : funcionarios) {
-            System.out.println("Funcionário: "+ func.getNome() + " - bônus: R$ " + func.calcularBonus());
+            System.out.println("SITUAÇÃO 1 - Funcionários criados com sucesso!");
+            for (Funcionario func : funcionarios) {
+                System.out.println("Funcionário: "+ func.getNome());
+            }
+
+            System.out.println("\nSITUAÇÃO 2 - Calcular bônus com sucesso!");
+            for (Funcionario func : funcionarios) {
+                System.out.println("Funcionário: "+ func.getNome() + " salário: R$ " + func.getSalario() + " - bônus: R$ " + func.calcularBonus());
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("\nSITUAÇÃO 3 - Tentar criar um funcionário com salário negativo");
+            Funcionario novoGerente = new Gerente("Diana", new BigDecimal("-500"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
